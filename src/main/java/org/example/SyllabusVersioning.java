66d590eb84e2c7d53d911522a173081e80f0789f
nimetts <105562039+nimetts@users.noreply.github.com>
2023-12-18 20:55:02 +0300

    Viewing selected version's content method is created

--- src/main/java/org/example/SyllabusVersioning.java
+++ src/main/java/org/example/SyllabusVersioning.java
@@ -12,4 +12,30 @@ public class SyllabusVersioning {
         this.time=time;
         this.lecture = lecture;
     }
+
+    @Override
+    public String toString() {
+        return "SyllabusVersioning{" +
+                "name='" + name + '\'' +
+                ", reason='" + reason + '\'' +
+                ", lecture=" + lecture +
+                ", time='" + time + '\'' +
+                '}';
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Lecture getLecture() {
+        return lecture;
+    }
 }
