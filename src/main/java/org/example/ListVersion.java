eb4f444fc29c553556ec1e07afc6ccaead555d49
MrEcho35 <yankiomac@hotmail.com>
2023-12-23 20:39:08 +0300

    ListVersion Update
    ListVersion now works more properly. User enters the course name and sees every single commit he made. User can also see info of its commit

--- src/main/java/org/example/ListVersion.java
+++ src/main/java/org/example/ListVersion.java
@@ -8,19 +8,26 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class ListVersion extends Application {
 
     private Stage primaryStage;
-    
+
     private ObservableList<String> fileNames;
+    private TextArea fileContentTextArea;
 
     @Override
     public void start(Stage primaryStage) {
@@ -32,47 +39,95 @@ public class ListVersion extends Application {
         ListView<String> versionListView = new ListView<>();
         versionListView.setPrefSize(300, 400);
 
+        TextField courseCodeTextField = new TextField();
+        courseCodeTextField.setPromptText("Enter Course Code");
 
-        Button chooseDirectoryButton = new Button("Choose Directory");
-        chooseDirectoryButton.setOnAction(e -> chooseDirectory());
-
-        fileNames = FXCollections.observableArrayList();
-        versionListView.setItems(fileNames);
+        Button listButton = new Button("List Versions");
+        listButton.setOnAction(e -> listVersions(courseCodeTextField.getText()));
 
         Button backButton = new Button("Back to Main Scene");
         backButton.setOnAction(e -> goBackToMainScene());
 
+        HBox searchBox = new HBox(10);
+        searchBox.setAlignment(Pos.CENTER);
+        searchBox.getChildren().addAll(courseCodeTextField, listButton);
+
+        fileNames = FXCollections.observableArrayList();
+        versionListView.setItems(fileNames);
+
+        fileContentTextArea = new TextArea();
+        fileContentTextArea.setEditable(false);
 
         VBox vBox = new VBox(10);
         vBox.setAlignment(Pos.CENTER);
         vBox.setPadding(new Insets(20));
-        vBox.getChildren().addAll(versionListView, chooseDirectoryButton, backButton);
+        vBox.getChildren().addAll(versionListView, searchBox, fileContentTextArea, backButton);
+
+        versionListView.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                displayFileContent(courseCodeTextField.getText(), newVal);
+            }
+        });
 
         borderPane.setCenter(vBox);
 
-        Scene scene = new Scene(borderPane, 400, 500);
+        Scene scene = new Scene(borderPane, 600, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     private void goBackToMainScene() {
+        
         FirstScene firstScene = new FirstScene();
         firstScene.start(primaryStage);
     }
 
-    private void chooseDirectory() {
+    private void listVersions(String courseCode) {
+        fileNames.clear();
 
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setTitle("Choose Directory");
-        File selectedDirectory = directoryChooser.showDialog(primaryStage);
+        // Construct the path to the directory based on the course code
+        String directoryPath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() +
+                File.separator + "Syllabus" + File.separator + courseCode;
 
-        if (selectedDirectory != null) {
+        File directory = new File(directoryPath);
 
-            File[] files = selectedDirectory.listFiles();
-            if (files != null) {
-                fileNames.setAll(Arrays.asList(files).stream().map(File::getName).toArray(String[]::new));
-            }
+        if (!directory.exists() || !directory.isDirectory()) {
+            showErrorAlert("Directory not found", "Directory does not exist: " + directory.getAbsolutePath());
+            return;
         }
+
+        // List all files with .json extension in the directory
+        File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(".json"));
+
+        if (files != null) {
+            Arrays.stream(files).map(File::getName).forEach(fileNames::add);
+        }
+    }
+
+    private void displayFileContent(String courseCode, String fileName) {
+        try {
+            // Construct the path based on the course code and file name
+            String filePath = FileSystemView.getFileSystemView().getDefaultDirectory().getPath() +
+                    File.separator + "Syllabus" + File.separator + courseCode + File.separator + fileName;
+
+            // Read the content of the file
+            StringBuilder fileContent = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    fileContent.append(line).append("\n");
+                }
+            }
+
+            // Display the content in the TextArea
+            fileContentTextArea.setText(fileContent.toString());
+        } catch (IOException e) {
+            showErrorAlert("Error", "Error reading file: " + e.getMessage());
+        }
+    }
+
+    private void showErrorAlert(String title, String content) {
+        // Implement logic to show an alert with the provided title and content
     }
 
     public static void main(String[] args) {
