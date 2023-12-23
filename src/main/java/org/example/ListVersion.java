c2905833b47529a6a61f467347d831f14e142117
MrEcho35 <yankiomac@hotmail.com>
2023-12-23 18:43:34 +0300

    ListVersion works now
    Now when in the list version screen, user can choose the relative directory to view all his content

--- src/main/java/org/example/ListVersion.java
+++ src/main/java/org/example/ListVersion.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,51 +10,43 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.List;
 
-import static javafx.application.Application.launch;
+import java.io.File;
+import java.util.Arrays;
 
-public class ListVersion extends Application{
+public class ListVersion extends Application {
 
     private Stage primaryStage;
-
-    // Use a list to store instances of App or EnglishSyllabus
-    private List<Application> versions;
+    
+    private ObservableList<String> fileNames;
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         primaryStage.setTitle("List of Versions");
 
-
         BorderPane borderPane = new BorderPane();
 
-
-        ListView<Application> versionListView = new ListView<>();
+        ListView<String> versionListView = new ListView<>();
         versionListView.setPrefSize(300, 400);
 
 
-        versions = new ArrayList<>();
+        Button chooseDirectoryButton = new Button("Choose Directory");
+        chooseDirectoryButton.setOnAction(e -> chooseDirectory());
 
+        fileNames = FXCollections.observableArrayList();
+        versionListView.setItems(fileNames);
 
-
-
-       // versions.add(new EnglishSyllabus());
-
-        ObservableList<Application> observableVersions = FXCollections.observableList(versions);
-        versionListView.setItems(observableVersions);
-
-        // Create a button to go back to the main scene
         Button backButton = new Button("Back to Main Scene");
         backButton.setOnAction(e -> goBackToMainScene());
 
-        // Create a VBox to hold the ListView and the Back button
+
         VBox vBox = new VBox(10);
         vBox.setAlignment(Pos.CENTER);
         vBox.setPadding(new Insets(20));
-        vBox.getChildren().addAll(versionListView, backButton);
+        vBox.getChildren().addAll(versionListView, chooseDirectoryButton, backButton);
 
         borderPane.setCenter(vBox);
 
@@ -62,10 +55,24 @@ public class ListVersion extends Application{
         primaryStage.show();
     }
 
-    public void goBackToMainScene() {
-
+    private void goBackToMainScene() {
         FirstScene firstScene = new FirstScene();
         firstScene.start(primaryStage);
+    }
+
+    private void chooseDirectory() {
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Choose Directory");
+        File selectedDirectory = directoryChooser.showDialog(primaryStage);
+
+        if (selectedDirectory != null) {
+
+            File[] files = selectedDirectory.listFiles();
+            if (files != null) {
+                fileNames.setAll(Arrays.asList(files).stream().map(File::getName).toArray(String[]::new));
+            }
+        }
     }
 
     public static void main(String[] args) {
